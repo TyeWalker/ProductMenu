7518057ca1329e94b369bb6f0b9f94d8576c5928
Tye <tyewalker09@gmail.com>
2021-08-08 13:32:40 +0200

    Added product class. Updated Menu list

---
+++ src/app/models/product.ts
@@ -0,0 +1,18 @@
+export class Product {
+    // Product attributes
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    image: string;
+
+    // Product Constructor:
+    constructor(id: number, name ='', description = '', price = 0, image = 'assets/cupcake-2.jpg') {
+        this.id = id
+        this.name = name
+        this.description = description
+        this.price = price
+        this.image = image;
+
+    }
+}
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Product } from 'src/app/models/product'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  
+  // TODO: Populate products from database and return Observable
+  products: Product[] = [
+    new Product(1, 'Product 1', 'P1 description, the product is tastey', 100),
+    new Product(2, 'Product 2', 'P2 description, the product is tastey', 300),
+    new Product(3, 'Product 3', 'P3 description, the product is tastey', 50),
+    new Product(4, 'Product 4', 'P4 description, the product is tastey', 20),
+    new Product(5, 'Product 5', 'P5 description, the product is tastey', 400),
+    new Product(6, 'Product 6', 'P6 description, the product is tastey', 40),
+  ]
 
   constructor() { }
+
+  getProducts(): Product[] {
+    return this.products
+  }
+
 }
